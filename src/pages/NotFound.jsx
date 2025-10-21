e897ad0d5a8c71e99a075ef3f361cd90b5ac8a4d
McAestro <mcaietherin@gmail.com>
2025-10-21 08:09:59 +0545

    11; added 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+    return () => {
+      document.body.style.overflow = 'auto'
+    };
+  }, [])
+
   return (
-    <>
-      test
-    </>
+    <section id='bidd'>
+      <div id="beige">
+        <h1>404</h1>
+        <h2>Something unexpected happened</h2>
+        <br />
+        <Link to={'/'}>Return home</Link>
+      </div>
+      <br />
+    </section>
   )
 }
 
